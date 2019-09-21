05bb49271bbfc81d1ba2eaf858d22c46cb92b247
Roberto Andrade <roberto.andrade@gmail.com>
2019-09-21 08:21:25 -0300

    Update build.ps1
    Restricting to only build developer

--- windows/build.ps1
+++ windows/build.ps1
@@ -1,8 +1,11 @@
 pushd $PSScriptRoot
 
-Get-ChildItem -Recurse -Filter dockerfile | foreach {
-	$tag = $_.Directory.Name
-	docker build -t $tag $_.DirectoryName
-}
+# Get-ChildItem -Recurse -Filter dockerfile | foreach {
+# 	$tag = $_.Directory.Name
+# 	docker build -t $tag $_.DirectoryName
+# }
+
+$tag = "mssql-server-windows-developer"
+docker build -t $tag $tag
 
 popd
